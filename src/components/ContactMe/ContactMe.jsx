dd8b7b1825eec8075dfc40ec07d30894fed640e1
Yuvraj <yuvrajsoneja2004@gmail.com>
2023-02-25 06:33:56 +0530

    whole nightB

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from './ContactMe.module.css';
 
 function ContactMe() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [msg, setMsg] = useState("");
+
     return (
         <div className={S.wrapper}>
             <h1>Contact Page</h1>
             <div className={S.map}>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15106.60110145241!2d82.701687727741!3d18.81373269582129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3af05dc359e0eb%3A0x659b6a7d5b4ce3db!2sKoraput%2C%20Odisha!5e0!3m2!1sen!2sin!4v1677284862372!5m2!1sen!2sin" width="600" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div className={S.form}>
+                <form onSubmit={e => e.preventDefault()}>
+                    <input type="text" placeholder='Enter your name' onChange={e => setName(e.target.value)} value={name} required />
+                    <input type="email" placeholder='Enter your email' onChange={e => setEmail(e.target.value)} value={email} required />
+                    <textarea cols="30" rows="10" placeholder='Enter your message' onChange={e => setMsg(e.target.value)} required value={msg}></textarea>
+                    <button type="submit">SEND</button>
+                </form>
+
             </div>
         </div>
     )
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -26,7 +26,7 @@ function NavigationBar() {
                         <Nav.Link className={S.navLink}><Link to='/products' className={S.noLink}><strong style={themeState}>Products</strong></Link></Nav.Link>
                         <Nav.Link className={S.navLink}><Link to='/products/sale' className={`${S.noLink} cumb`} > <span className="spanSale">Sale</span><strong style={themeState}>Sale</strong></Link></Nav.Link>
                         <Nav.Link className={S.navLink}><Link to='/' className={S.noLink}><strong style={themeState}>About</strong></Link></Nav.Link>
-                        <Nav.Link className={S.navLink}><Link to='/' className={S.noLink}><strong style={themeState}>Contact</strong></Link></Nav.Link>
+                        <Nav.Link className={S.navLink}><Link to='/contact' className={S.noLink}><strong style={themeState}>Contact</strong></Link></Nav.Link>
                         <div className={S.sm}>
                             <span
 
@@ -46,14 +46,6 @@ function NavigationBar() {
 
                             >
                                 <CiSearch size={26} style={themeState} className={S.navIcons} />
-                            </Link>
-                            <Link
-                                to='/wishlist'
-                                style={themeState}
-                                className={S.navIcons}
-
-                            >
-                                <CiHeart size={26} style={themeState} className={S.navIcons} />
                             </Link>
 
                             <div className={S.cartIcon} style={{ position: 'relative', display: 'inline', ...themeState }} >
